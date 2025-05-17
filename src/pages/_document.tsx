107821a314785b3d7cb655ca8b4fdb7fec5b2532
Abhishek Singh <singh.abhishek1117@gmail.com>
2025-05-17 23:09:55 +0530

    url image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,6 +21,16 @@ class MyDocument extends Document<DocumentProps> {
           <link rel="icon" href="/favicon.ico" />
           <meta name="viewport" content="initial-scale=1, width=device-width" />
 
+          {/* ✅ Open Graph and Twitter meta tags */}
+          <meta property="og:title" content="Smile Age India" />
+          <meta property="og:description" content="Smile Age India (SAI) - A platform for..." />
+          <meta property="og:image" content="https://www.smileageindia.com/logo.png" />
+          <meta property="og:url" content="https://www.smileageindia.com/" />
+          <meta property="og:type" content="website" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:image" content="https://www.smileageindia.com/logo.jpg" />
+
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.background.paper} />
 
@@ -47,35 +57,9 @@ class MyDocument extends Document<DocumentProps> {
   }
 }
 
-// `getInitialProps` belongs to `_document` (instead of `_app`),
-// it's compatible with static-site generation (SSG).
 MyDocument.getInitialProps = async (ctx: DocumentContext) => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
-
   const originalRenderPage = ctx.renderPage
 
-  // You can consider sharing the same emotion cache between all the SSR requests to speed up performance.
-  // However, be aware that it can have global side effects.
   const cache = createEmotionCache()
   const { extractCriticalToChunks } = createEmotionServer(cache)
 
@@ -87,14 +71,11 @@ MyDocument.getInitialProps = async (ctx: DocumentContext) => {
         App: NextComponentType<AppContextType, AppInitialProps, AppPropsType & { emotionCache: EmotionCache }>
       ) =>
         function EnhanceApp(props) {
-          // console.log('props ->', props)
           return <App emotionCache={cache} {...props} />
         },
     })
 
   const initialProps = await Document.getInitialProps(ctx)
-  // This is important. It prevents emotion to render invalid HTML.
-  // See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
   const emotionStyles = extractCriticalToChunks(initialProps.html)
   const emotionStyleTags = emotionStyles.styles.map((style) => (
     <style
